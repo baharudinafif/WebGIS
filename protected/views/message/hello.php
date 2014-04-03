5f43c739fd2aff5b9e0b9b90c6f216bcf60174cc
baharudinafif <baharudin.afif@ymail.com>
2014-04-03 14:57:24 +0700

    coba-coba

--- protected/views/message/hello.php
+++ protected/views/message/hello.php
@@ -1,14 +1,20 @@
 <?php
 /* @var $this MessageController */
 
-$this->breadcrumbs=array(
-	'Message'=>array('/message'),
-	'Hello',
-);
+	$this->breadcrumbs=array('Message'=>array('/message'),'Hello',);
+	$connection = Yii::app()->db;
 ?>
 <h1><?php echo "Hello World!!"?></h1>
 
 <?php
 	echo $time;
+	$result=$connection->createCommand()
+		-> select('*')
+		-> from ('Gunung')
+		-> query();
+	foreach($result as $row){
+		echo "<br>" . $row['Nama']." Ketinggian : " . $row['Ketinggian'];
+	}
 	echo CHtml::link('<br>Go To : GoodBye',array('message/goodbye')); 
+	
 ?>
